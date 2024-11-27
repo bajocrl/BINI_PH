5d45255d5c85714f07891083ba18291b2fcca4ab
bajocrl <shgmkaze@gmail.com>
2024-11-27 10:17:21 +0800

    vercel issue fixed

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { createRoot } from 'react-dom/client';
 import { Page } from '@/pages';
 import '@/styles/index.css';
 
-import { SpeedInsights } from '@vercel/speed-insights/next';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 createRoot(document.getElementById('root')!).render(
   <>
